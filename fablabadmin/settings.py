badb4f28e27f5eb431523c467b68b12af44cbacc
Boris Fritscher <boris@fritscher.ch>
2016-01-28 19:40:16 +0100

    invoice number format

--- fablabadmin/settings.py
+++ fablabadmin/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
