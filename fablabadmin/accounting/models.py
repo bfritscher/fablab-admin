25d17ebabd94d6b0f01be4d45fa89f21ff0da308
Boris Fritscher <boris@fritscher.ch>
2017-03-26 16:58:10 +0200

    support consolidation virement étranger closes #65

--- fablabadmin/accounting/models.py
+++ fablabadmin/accounting/models.py
@@ -13,6 +13,10 @@ virement_du_compte = re.compile(ur"VIREMENT DU COMPTE ([\d-]*) (.*?)(?: EXPÉDIT
 # VIREMENT DE SIC ONLINE (SIC_IID) DONNEUR D'ORDRE: (nom) (iban) [COMMUNICATIONS: (text)]
 virement_de_sic = re.compile(ur"VIREMENT DE SIC ONLINE ([\d]*) DONNEUR D'ORDRE: (.*?)( [\dA-Z]+)(?: COMMUNICATIONS: (.*?))$")
 
+#VIREMENT ÉTRANGER NO DE RÉF. (ref) EXPÉDITEUR: (nom) EXPÉDITEUR BANQUE: (banque) MOTIF DE PAIEMENT: (text)
+
+virement_etranger = re.compile(ur"VIREMENT ÉTRANGER NO DE RÉF\. ([\dA-Z]*) EXPÉDITEUR: (.*?)(?:EXPÉDITEUR BANQUE: (.*?))(?: MOTIF DE PAIEMENT: (.*?))$")
+
 # DBIT
 # E-FINANCE (compte) (nom) [(iban) (nom adresse)]
 e_finance = re.compile(ur"E-FINANCE ([\d-]*) (.*?)(?:( [\dA-Z]{20,34} )(.*?))?$")
@@ -69,6 +73,15 @@ class BankTransaction(models.Model):
                     o.contact = res[1]
                     o.iban = res[2]
                     o.message = res[3]
+                else:
+                    m = virement_etranger.match(smart_text(self.raw_text))
+                    if m is not None:
+                        res = m.groups()
+                        o.type = 'ETRANGER'
+                        o.ref = res[0]
+                        o.contact = res[1]
+                        o.banque = res[2]
+                        o.message = res[3]
 
         if self.type == BankTransaction.DEBIT:
             m = e_finance.match(smart_text(self.raw_text))
